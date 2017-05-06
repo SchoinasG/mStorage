49d08fca791fee72c9b90210530f0bcce2834b56
Schoinas- home <gs@itrust.gr>
2017-05-06 13:19:30 +0300

    Added empty activities for 4 buttons and linked the buttons

--- app/src/main/java/com/example/johnnie/mstorage/HomeActivity.java
+++ app/src/main/java/com/example/johnnie/mstorage/HomeActivity.java
@@ -1,7 +1,9 @@
 package com.example.johnnie.mstorage;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -9,5 +11,21 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+    }
+    public void onClickImport(View view){
+        Intent intent = new Intent(this, ImportData.class);
+        startActivity(intent);
+    }
+    public void onClickView(View view){
+        Intent intent = new Intent(this, ViewData.class);
+        startActivity(intent);
+    }
+    public void onClickAudit(View view){
+        Intent intent = new Intent(this, StartAudit.class);
+        startActivity(intent);
+    }
+    public void onClickReport(View view){
+        Intent intent = new Intent(this, MakeReport.class);
+        startActivity(intent);
     }
 }
