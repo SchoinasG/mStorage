c77b4f612d3dc5e55b181bd4432800a4415bf301
Masterace <telisjok@yahoo.com>
2017-06-12 01:21:30 +0300

    Add Item Functionality (Telis)

--- app/src/main/java/com/example/johnnie/mstorage/Audit/ItemAudit.java
+++ app/src/main/java/com/example/johnnie/mstorage/Audit/ItemAudit.java
@@ -10,12 +10,15 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.KeyEvent;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.johnnie.mstorage.AddItem;
 import com.example.johnnie.mstorage.DBContract;
 import com.example.johnnie.mstorage.DBHandler;
 import com.example.johnnie.mstorage.Item;
@@ -169,5 +172,23 @@ public class ItemAudit extends AppCompatActivity implements ZXingScannerView.Res
             Qrview.resumeCameraPreview(this);
         }
 
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.audit_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch(item.getItemId()){
+            case R.id.addItem:
+                i = new Intent(this, AddItem.class);
+                startActivity(i);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
--- app/src/main/java/com/example/johnnie/mstorage/JasonParser.java
+++ app/src/main/java/com/example/johnnie/mstorage/JasonParser.java
@@ -4,6 +4,7 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Masterace on 28/5/2017.
@@ -75,6 +76,39 @@ public class JasonParser {
         }
 
         return  ItemsList;
+    }
+
+    public static ArrayList<Department> parseDepartments(JSONObject jsonresponse) throws JSONException{
+        ArrayList<Department> Departments = new ArrayList<>();
+
+        //Bunch of attributes we're gonna need
+        int loops = 0;
+        String DepartmentName;
+        int DepartmentID, BelongsToStorage;
+
+        JSONObject Storages = jsonresponse; //Get Jason object and store it in Storages
+        loops = Storages.getInt("numberOfResults"); //Get number of storages the above object contains
+        JSONArray storageArray = Storages.getJSONArray("wrappedResponse"); //Get the data for each storage
+
+        for(int i=0; i<loops; i++){ //Loop through each storage and extract the data we want
+            JSONObject storageData = storageArray.getJSONObject(i); //Extracting data for the i storage found in storageArray array
+
+            JSONArray departmentArray = storageData.getJSONArray("departments"); //Extracting the whole array for the departments of the current (i) storage
+            int loopsForDepartments = departmentArray.length(); //Counting how many times we are going to loop for the departments data to extract them
+
+            if(loopsForDepartments > 0){ //If there are no departments at all in the storage, don't enter
+                for(int j=0; j<loopsForDepartments; j++){ //If there's at least one department then start looping
+                    JSONObject departmentData = departmentArray.getJSONObject(j); //Extracting data for the i department found in departmentArray array
+
+                    DepartmentID = departmentData.getInt("id"); //Extracting the id
+                    DepartmentName = departmentData.getString("name"); //Extracting the name
+                    BelongsToStorage = departmentData.getInt("storageId"); //Extracting the id of the storage that the department belongs to
+
+                    Departments.add(new Department(DepartmentID, BelongsToStorage, DepartmentName)); //Store the i department's data in the DepartmentsArray (which is an ObjectArray containing objects of type Department) using the Department's constructor (See Department class)
+                }
+            }
+        }
+        return Departments; //After the loop has finished, return the ObjectArray which is named StoragesArray
     }
 
 }
