f215b95d458e24c0309263caff69f47ebc1b59aa
Masterace <telisjok@yahoo.com>
2017-06-12 09:59:04 +0300

    Final Pre-Presentation Improvements (Telis)

--- app/src/main/java/com/example/johnnie/mstorage/AddItem.java
+++ app/src/main/java/com/example/johnnie/mstorage/AddItem.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -37,42 +38,46 @@ public class AddItem extends AppCompatActivity {
 
     }
 
-
-
     public void addItem(View v){
-        if(Name.length()>0 && Description.length()>0 && Category.length()>0 && Position.length()>0 && SKU.length()>0 && Quantity.length()>0 && InDepartment.length()>0 &&
-                Mes.length()>0){
-            dbHandler = new DBHandler(this);
 
-            ItemName = Name.getText().toString();
-            ItemDesc = Description.getText().toString();
-            ItemCat = Category.getText().toString();
-            ItemPos = Position.getText().toString();
-            ItemSKU = SKU.getText().toString();
-            MesUnit = Mes.getText().toString();
-            ItemQuantity = Integer.parseInt(Quantity.getText().toString());
-            ItemInDepartment = Integer.parseInt(InDepartment.getText().toString());
+        try{
+            if(Name.length()>0 && Description.length()>0 && Category.length()>0 && Position.length()>0 && SKU.length()>0 &&
+                    Quantity.length()>0 && InDepartment.length()>0 && Mes.length()>0){
+                dbHandler = new DBHandler(this);
 
-            if(dbHandler.AddItem(ItemName, ItemDesc, ItemCat, ItemPos, ItemSKU, MesUnit, ItemQuantity, ItemInDepartment)){
-                Toast.makeText(getApplicationContext(), "Item Added", Toast.LENGTH_LONG).show();
-                i = new Intent(this, ItemAudit.class);
-                startActivity(i);
+                ItemName = Name.getText().toString();
+                ItemDesc = Description.getText().toString();
+                ItemCat = Category.getText().toString();
+                ItemPos = Position.getText().toString();
+                ItemSKU = SKU.getText().toString();
+                MesUnit = Mes.getText().toString();
+                ItemQuantity = Integer.parseInt(Quantity.getText().toString());
+                ItemInDepartment = Integer.parseInt(InDepartment.getText().toString());
+
+                if(dbHandler.AddItem(ItemName, ItemDesc, ItemCat, ItemPos, ItemSKU, MesUnit, ItemQuantity, ItemInDepartment)){
+                    Toast.makeText(getApplicationContext(), "Item Added", Toast.LENGTH_LONG).show();
+                    i = new Intent(this, ItemAudit.class);
+                    startActivity(i);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Error adding item. Please try again.", Toast.LENGTH_LONG).show();
+                    Name.setText("");
+                    Description.setText("");
+                    Category.setText("");
+                    Position.setText("");
+                    SKU.setText("");
+                    Quantity.setText("");
+                    InDepartment.setText("");
+                    Mes.setText("");
+                }
             } else {
-                Toast.makeText(getApplicationContext(), "Error adding item. Please try again.", Toast.LENGTH_LONG).show();
-                Name.setText("");
-                Description.setText("");
-                Category.setText("");
-                Position.setText("");
-                SKU.setText("");
-                Quantity.setText("");
-                InDepartment.setText("");
-                Mes.setText("");
+                Toast.makeText(getApplicationContext(), "Please fill in all the fields and then tap on Add again.", Toast.LENGTH_LONG).show();
             }
-        } else {
-            Toast.makeText(getApplicationContext(), "Please fill in all the fields and then tap on Add again.", Toast.LENGTH_LONG).show();
+            Log.i("", ItemQuantity +  "Is int" );
+            Log.i("", ItemInDepartment +  "Is int" );
+        } catch(NumberFormatException e) {
+            Log.i("", "Item quantity or ItemInDepartment is not int");
+            Toast.makeText(getApplicationContext(), "Please check Item Quantity and In Department get INTEGER values only.", Toast.LENGTH_LONG).show();
         }
-
-
 
     }
 }
